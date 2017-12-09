1fd033319c037947e213c78fb95d2898d0c2a65e
Unknown <1084714805@qq.com>
2017-12-09 19:39:02 +0800

    finish Character Control

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
 
+    public float RotateSpeed;
+    public float InvertSpeed;
     private Animator animator;
+
+    private float invert = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +19,23 @@ public class CharacterController : MonoBehaviour {
 	void Update () {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
+        float inputRun = Input.GetAxis("Run");
 
-        animator.SetFloat("BlendX", inputX);
-        animator.SetFloat("BlendY", inputY);
+        inputRun += 1;
+
+        if (inputY < 0) {
+            invert = Mathf.Max(invert - InvertSpeed * Time.deltaTime, -1.0f);
+        } else if (inputY > 0) {
+            invert = Mathf.Min(invert + InvertSpeed * Time.deltaTime, 1.0f);
+        }
+
+        animator.SetFloat("BlendX", inputX * inputRun * invert);
+        animator.SetFloat("BlendY", inputY * inputRun);
+
+        if (Mathf.Abs(inputY) < 0.1f) return;
+
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(0, inputY);
+        Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotateSpeed * Time.deltaTime);
     }
 }
