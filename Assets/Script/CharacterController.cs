d45d3256b7b00c11eed197758f4e359a033c7c0a
Unknown <1084714805@qq.com>
2017-12-09 15:13:18 +0800

    adjust

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        animator.SetFloat("BlendX", inputX);
+        animator.SetFloat("BlendY", inputY);
+    }
+}
